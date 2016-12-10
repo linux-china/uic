9e83b76b3d7fb08e9e5b1f72da5a5faaa0e46abb
linux_china <linux_china@hotmail.com>
2016-12-10 12:13:36 +0800

    user product line application

---
+++ src/main/java/org/mvnsearch/uic/UserProductLineApplication.java
@@ -0,0 +1,17 @@
+package org.mvnsearch.uic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * user product line application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class UserProductLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProductLineApplication.class, args);
+	}
+}
