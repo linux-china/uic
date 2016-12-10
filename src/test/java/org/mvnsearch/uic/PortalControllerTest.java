0da2b71e90ea34439370a2233e13fa391a32ba6d
linux_china <linux_china@hotmail.com>
2016-12-10 12:26:16 +0800

    Polishing

--- src/test/java/org/mvnsearch/uic/PortalControllerTest.java
+++ src/test/java/org/mvnsearch/uic/PortalControllerTest.java
@@ -3,6 +3,7 @@ package org.mvnsearch.uic;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
