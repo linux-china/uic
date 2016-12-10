debdc9d73ac07618f90ec1565573f961f574413a
linux_china <linux_china@hotmail.com>
2016-12-10 17:11:47 +0800

    moved

--- src/test/java/org/mvnsearch/uic/PortalControllerTest.java
+++ src/test/java/org/mvnsearch/uic/PortalControllerTest.java
@@ -2,11 +2,9 @@ package org.mvnsearch.uic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mvnsearch.uic.login.PortalController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
