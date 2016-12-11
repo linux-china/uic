20f3d76b6c44e806e155b5869b8246390020db6b
linux_china <linux_china@hotmail.com>
2016-12-11 17:33:41 +0800

    audition event

---
+++ src/main/java/org/mvnsearch/uic/domain/event/AuditionEvent.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.uic.domain.event;
+
+import org.mvnsearch.ddd.domain.events.BaseDomainEvent;
+
+/**
+ * audition event
+ *
+ * @author linux_china
+ */
+public class AuditionEvent extends BaseDomainEvent<Object> {
+
+
+}
