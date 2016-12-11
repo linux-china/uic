47bf53724d41e1cf113cddacd00590f91d808590
linux_china <linux_china@hotmail.com>
2016-12-11 13:26:28 +0800

    编辑备注描述

--- src/main/java/org/mvnsearch/uic/login/PortalController.java
+++ src/main/java/org/mvnsearch/uic/login/PortalController.java
@@ -1,5 +1,7 @@
 package org.mvnsearch.uic.login;
 
+import org.mvnsearch.uic.domain.application.AccountManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class PortalController {
+    @Autowired
+    private AccountManager accountManager;
+    //不能引用Domain内部的组件，如repository, service, infrastructure等
+
     @GetMapping("/")
     @ResponseBody
     public String index() {
